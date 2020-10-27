f2bd94abe94e6f3cfee12475a7196f46e24dd268
David Cooper <dmcooper2412@gmail.com>
2020-10-27 16:42:30 -0400

    event to have bot send requests out when a user is speaking

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const axios = require('axios');
 const client = new Discord.Client();
 
 client.on('ready', async () => {
@@ -8,8 +9,36 @@ client.on('ready', async () => {
     // must run after we login
     const testChamberChannel = await client.channels.fetch(process.env.CHANNEL_ID);
     testChamberChannel.join().then(connection => {
-        connection.on('speaking', (user, isSpeaking) => {
-            console.log(`${user.username} is speaking? `, isSpeaking.bitfield === 1);
+        connection.on('speaking', async (discordUser, speaking) => {
+
+            const user = {
+                user: discordUser.username,
+                isSpeaking: speaking.bitfield === 1
+            }
+
+            console.log(`${discordUser.username} is speaking? `, speaking.bitfield === 1);
+            // send a POST req to another web server 
+            /**
+             * {
+             *   "user": user.username
+             *   "isSpeaking": isSpeaking.bitfield === 1
+             * }
+             * 
+             * example :
+             * {
+             *   "user": "david",
+             *   "isSpeaking": true | false
+             * }
+             */
+            try {
+                
+                const options = {
+                    headers: {'Content-Type': 'application/json'}
+                };
+                const response = await axios.post('http://raspberrypi.local:3000/', user, options);
+            } catch (error) {
+                console.error(error);
+            }
         })
     })
 });
